8c66b70d9072eec060fe2b968529408c1577e819
alice ky <aya43@alicedeMacBook.local>
2021-05-18 10:24:22 -0700

    update

---
+++ labs/01/files_test/p1.c
@@ -0,0 +1,13 @@
+// author: <First name, last name; YOUR SFU USER ID HERE>
+// date: 2021-05-18
+// input: void
+// output: int
+// description: prints a greeting to standard output.
+// this will NOT be graded.
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
