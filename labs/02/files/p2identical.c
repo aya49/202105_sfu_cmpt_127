c88ce0a08c3b1db74c548094c56e06fc364d4ab2
aya49 <alice.ky@live.ca>
2021-06-01 13:31:09 -0700

    update

--- labs/02/files/p2identical.c
+++ labs/02/files/p2identical.c
@@ -1,13 +1,5 @@
 int identical(int arr1[], int arr2[], unsigned int len) {
-    if (len == 0) {
-        return 1;
-    }
-    for (int i=0; i<len; i++) {
-        if (arr1[i] != arr2[i]) {
-            return 0;
-        }
-    }
-    return 1;
+    // FILL IN BODY
 }
 
 int scrambled(int arr1[], int arr2[], unsigned int len) {
