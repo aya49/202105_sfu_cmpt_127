df0f6e580752ad9532cfead5ae5747dc36fbc0c6
aya49 <alice.ky@live.ca>
2021-07-06 16:48:40 -0700

    a4

--- assignments/02/files/t3.c
+++ assignments/02/files/t3.c
@@ -3,3 +3,16 @@
 // input: 
 // output: 
 // description: 
+
+#include <stdio.h>
+
+int main(int argc, char* argv) {
+    // argc = 3
+    // argv array of 3 char arrays
+
+    //a whole lot of code
+
+    return 0;
+}
+
+// input --> function 
